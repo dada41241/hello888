56a85aac120950bb1acf134ebb3c016eab92d985
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-06-05 23:21:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
       
-    if event.message.text == "每日新知":
+    if event.message.text == "1":
         content = test_news()
         line_bot_api.reply_message(
             event.reply_token,
