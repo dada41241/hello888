ab987abd4fe2f942941b481c410ebfe302b51f4d
dada41241 <32788226+dada41241@users.noreply.github.com>
2020-06-05 23:32:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def handle_message(event):
         return 0
       
     if event.message.text == "1":
-        content = random.choices(testnews(),k=5)
+        content = random.choices(test_news(),k=5)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
